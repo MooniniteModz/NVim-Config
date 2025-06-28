e54678c7b16e6345d40208f1f13814eb10a34776
Moon()Y <mooninitemodz@gmail.com>
2025-06-27 21:25:19 -0500

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,4 @@
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -5,12 +6,18 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
-                            
-local opts = {}
-require("vim-options")
-require("lazy").setup("plugins")
+
+-- Leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Load core configuration
+require("config.options")
+require("config.lazy")
+require("config.keymaps")
+require("config.autocmds")
