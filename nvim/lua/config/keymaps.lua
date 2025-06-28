7fb742099ecd9a9742a4aa4d90624228bb8ba29b
Moon()Y <mooninitemodz@gmail.com>
2025-06-27 21:30:36 -0500

    Create keymaps.lua

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,79 @@
+local keymap = vim.keymap
+
+-- General keymaps
+keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Explorer" })
+
+-- Move text up and down
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move text down" })
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move text up" })
+
+-- Better joining lines
+keymap.set("n", "J", "mzJ`z")
+
+-- Center cursor when moving
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+
+-- Greatest remap ever (paste without yanking)
+keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste without yanking" })
+
+-- Next greatest remap ever (delete without yanking)
+keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without yanking" })
+
+-- Yank to system clipboard
+keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to clipboard" })
+keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line to clipboard" })
+
+-- Format file
+keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format file" })
+
+-- Quick fix navigation
+keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Replace word under cursor
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace word" })
+
+-- Make file executable
+keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make executable" })
+
+-- Window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Equal splits" })
+keymap.set("n", "<leader>sx", ":close<CR>", { desc = "Close split" })
+
+-- Navigate between splits
+keymap.set("n", "<C-h>", "<C-w>h")
+keymap.set("n", "<C-j>", "<C-w>j")
+keymap.set("n", "<C-k>", "<C-w>k")
+keymap.set("n", "<C-l>", "<C-w>l")
+
+-- Resize splits
+keymap.set("n", "<C-Up>", ":resize +2<CR>")
+keymap.set("n", "<C-Down>", ":resize -2<CR>")
+keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
+keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
+
+-- Tab management
+keymap.set("n", "<leader>to", ":tabnew<CR>", { desc = "New tab" })
+keymap.set("n", "<leader>tx", ":tabclose<CR>", { desc = "Close tab" })
+keymap.set("n", "<leader>tn", ":tabn<CR>", { desc = "Next tab" })
+keymap.set("n", "<leader>tp", ":tabp<CR>", { desc = "Previous tab" })
+
+-- Stay in indent mode
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
+
+-- Disable arrow keys
+keymap.set("n", "<Up>", "<Nop>")
+keymap.set("n", "<Down>", "<Nop>")
+keymap.set("n", "<Left>", "<Nop>")
+keymap.set("n", "<Right>", "<Nop>")
+```
+
+---
