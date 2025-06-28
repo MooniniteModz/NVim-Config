ba021bf7929796d6a0753e57851547a1f1a6e89c
Moon()Y <mooninitemodz@gmail.com>
2025-06-27 21:29:47 -0500

    Create lazy.lua

---
+++ nvim/lua/config/lazy.lua
@@ -0,0 +1,22 @@
+require("lazy").setup({
+  spec = {
+    { import = "plugins" },
+  },
+  defaults = {
+    lazy = false,
+    version = false, -- always use the latest git commit
+  },
+  install = { colorscheme = { "tokyonight", "catppuccin" } },
+  checker = { enabled = true, notify = false },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
