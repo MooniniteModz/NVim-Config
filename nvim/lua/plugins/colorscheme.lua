6f755234485008a088eb1fccd74cabd9b3b41b3c
Moon()Y <mooninitemodz@gmail.com>
2025-06-27 21:32:00 -0500

    Create colorscheme.lua

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,52 @@
+return {
+  -- Tokyonight (default)
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        style = "night",
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          comments = { italic = false },
+          keywords = { italic = false },
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+      })
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+
+  -- Alternative: Catppuccin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    enabled = false,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true,
+      })
+    end,
+  },
+
+  -- Alternative: Rose Pine
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    enabled = false,
+    config = function()
+      require("rose-pine").setup({
+        variant = "moon",
+        dark_variant = "moon",
+        disable_background = true,
+      })
+    end,
+  },
+}
+```
+
+---
